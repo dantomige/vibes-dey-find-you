2de84b888357b618108afc005816977e4b7f0ea0
Daniel Ige <dantomige@gmail.com>
2026-04-04 18:42:41 -0700

    refactor: rename factors, add back population

--- src/db/base.py
+++ src/db/base.py
@@ -3,28 +3,35 @@ from sqlalchemy import Table, Column, ForeignKey, Integer, String
 
 Base = declarative_base()
 
-song_artists = Table(
+song_artists_table = Table(
     "song_artists",
     Base.metadata,
     Column("song_id", ForeignKey("songs.id"), primary_key=True),
-    Column("song_id", ForeignKey("artists.id"), primary_key=True)
+    Column("artist_id", ForeignKey("artists.id"), primary_key=True),
 )
 
-class Song(Base):
+
+class SongModel(Base):
 
     __tablename__ = "songs"
 
     id = Column(String, primary_key=True)
     mbid = Column(String, nullable=False)
     title = Column(String, nullable=False)
-    artists = relationship("Artist", secondary=song_artists)
     duration = Column(Integer, nullable=True)
     isrc = Column(String, nullable=True)
 
-class Artist(Base):
+    artists = relationship("ArtistModel", secondary=song_artists_table)
+
+
+class ArtistModel(Base):
 
     __tablename__ = "artists"
 
     id = Column(String, primary_key=True)
     arid = Column(String, nullable=False)
     name = Column(String, nullable=False)
+
+    songs = relationship(
+        "SongModel", secondary=song_artists_table, back_populates="artists"
+    )
