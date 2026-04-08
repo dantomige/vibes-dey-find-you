e14c1c2c1083f079aea0a20d82b726e4ff576a6a
Daniel Ige <dantomige@gmail.com>
2026-04-07 19:23:12 -0700

    bug: wrong attribute name

--- src/client/base.py
+++ src/client/base.py
@@ -3,7 +3,7 @@ from abc import abstractmethod
 class BaseClient:
 
     def __init__(self, headers):
-        self.header = headers
+        self.headers = headers
 
     @abstractmethod
     def get(self, endpoint):
