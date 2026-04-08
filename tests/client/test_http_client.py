5527521afb4588988ef37ac985aaabb8486b5d46
Daniel Ige <dantomige@gmail.com>
2026-04-07 19:24:12 -0700

    test: refactor request call in http client test case

--- tests/client/test_http_client.py
+++ tests/client/test_http_client.py
@@ -4,6 +4,7 @@ from src.client.http_client import HTTPClient
 def test_http_client_get():
 
     headers = {"User-Agent": "test-script"}
+    url = "http://example.com"
 
     client = HTTPClient(headers=headers)
 
@@ -13,7 +14,7 @@ def test_http_client_get():
 
     with patch("requests.get", return_value=mock_response) as mock_get:
         response = client.get("http://example.com")
-        mock_get.assert_called_once_with("http://example.com")
+        mock_get.assert_called_once_with(url=url, headers=headers)
 
         assert response.status_code == 200
         assert response.json() == {"message": "ok"}
