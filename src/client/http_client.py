985f29c67b56e93b7a0f4a3e6fbdf22be707932f
Daniel Ige <dantomige@gmail.com>
2026-04-07 19:23:37 -0700

    feat: http_client implementation

--- src/client/http_client.py
+++ src/client/http_client.py
@@ -4,5 +4,5 @@ from src.client.base import BaseClient
 
 class HTTPClient(BaseClient):
     
-    def get(self, endpoint):
-        raise NotImplementedError
+    def get(self, url):
+        return requests.get(url=url, headers=self.headers)
