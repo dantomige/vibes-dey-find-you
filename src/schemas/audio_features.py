73401108f41c1984ccc7f2d17905a85a34f18e14
Daniel Ige <dantomige@gmail.com>
2026-04-15 05:07:27 -0700

    feat: add audio features schema

---
+++ src/schemas/audio_features.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class AudioFeatures(BaseModel):
+    acousticness: float
+    danceability: float
+    energy: float
+    instrumentalness: float
+    key: int
+    liveness: float
+    loudness: float
+    mode: int
+    speechiness: float
+    tempo: float
+    valence: float
