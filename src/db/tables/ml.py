04ecf0045f25aec44febe18c37c497c541f086b0
Daniel Ige <dantomige@gmail.com>
2026-04-15 05:26:43 -0700

    refactor: move table into tables folder

---
+++ src/db/tables/ml.py
@@ -0,0 +1,37 @@
+from src.db.base import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
+
+song_artists_table = Table(
+    "song_artists",
+    Base.metadata,
+    Column("song_id", ForeignKey("songs.id"), primary_key=True),
+    Column("artist_id", ForeignKey("artists.id"), primary_key=True),
+)
+
+
+class SongModel(Base):
+
+    __tablename__ = "songs"
+
+    id = Column(String, primary_key=True)
+    rid = Column(String, unique=True, nullable=False)
+    mbid = Column(String, unique=True, nullable=False)
+    title = Column(String, nullable=False)
+    duration = Column(Integer, nullable=True)
+    isrc = Column(String, nullable=True)
+
+    artists = relationship("ArtistModel", secondary=song_artists_table)
+
+
+class ArtistModel(Base):
+
+    __tablename__ = "artists"
+
+    id = Column(String, primary_key=True)
+    arid = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+
+    songs = relationship(
+        "SongModel", secondary=song_artists_table, back_populates="artists"
+    )
