092e05e8aa512c8a33fe15ddf386a19862377f22
Daniel Ige <dantomige@gmail.com>
2026-04-15 05:25:57 -0700

    bug: fixed wrong folder name in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from pathlib import Path
 @pytest.fixture
 def load_fixture():
     def _load(name):
-        path = Path(__file__).parent / "fixture" / name
+        path = Path(__file__).parent / "fixtures" / name
         with open(path) as f:
             return json.load(f)
     return _load
