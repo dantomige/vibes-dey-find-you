513c56ecaafa0d9d6e4d851f6fb65f86b10211f4
Daniel Ige <dantomige@gmail.com>
2026-04-15 07:21:04 -0700

    refactor: update song repository spec

--- src/db/crud/core.py
+++ src/db/crud/core.py
@@ -1,25 +1,40 @@
+import pandas as pd
 from src.schemas.song import Song
 from src.db.tables.core import SongModel, ArtistModel, song_artists_table
+
 
 class SongRepository:
 
     def __init__(self, db):
         self.db = db
 
-    def add_songs(self, songs: list[Song]):
-        raise NotImplementedError
-    
-    def get_song(self, rid: str):
-        raise NotImplementedError
-    
-    def get_all_songs(self) -> list[Song]:
-        raise NotImplementedError
-    
-    def update_song(self, rid: str, song: Song):
-        raise NotImplementedError
-    
-    def remove_song(self, rid: str):
+    def get_song_ids_by_external_id(
+        self,
+        isrc: str | None = None,
+        music_brainz_id: str | None = None,
+        recco_beats_id: str | None = None,
+    ) -> str | None:
+        """
+        Fetches the song ID corresponding to the given external
+        IDs (music_brainz_id, recco_beats_id, isrc).
+        Returns the song ID if a match is found, or None if no match is found.
+        """
         raise NotImplementedError
 
-    def to_dataframe(self):
+    def add_songs(self, songs: list[Song]) -> list[str]:
+        raise NotImplementedError
+
+    def get_song(self, song_id: str) -> SongModel | None:
+        raise NotImplementedError
+
+    def get_all_songs(self) -> list[SongModel]:
+        raise NotImplementedError
+
+    def update_song(self, song_id: str, song: Song):
+        raise NotImplementedError
+
+    def remove_song(self, song_id: str):
+        raise NotImplementedError
+
+    def to_dataframe(self) -> pd.DataFrame:
         raise NotImplementedError
