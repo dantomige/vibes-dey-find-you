fa641be113f2bddabd72ce7f3eb908979c0a2453
Daniel Ige <dantomige@gmail.com>
2026-04-15 22:18:53 -0700

    feat: audio features repo

--- src/db/crud/ml.py
+++ src/db/crud/ml.py
@@ -1,6 +1,30 @@
+import pandas as pd
+from src.db.tables.ml import AudioFeaturesModel
 from src.schemas.audio_features import AudioFeatures
 
-class FeatureRepository:
+
+class AudioFeaturesRepository:
 
     def __init__(self, db):
         self.db = db
+
+    def get_audio_features(self, song_id: str) -> AudioFeaturesModel | None:
+        raise NotImplementedError
+
+    def add_audio_features(
+        self, song_id: str, features: AudioFeatures
+    ) -> AudioFeaturesModel:
+        raise NotImplementedError
+
+    def update_audio_features(
+        self, song_id: str, features: AudioFeatures
+    ) -> AudioFeaturesModel:
+        """Updates the audio features for the given song ID. If no audio features exist for the song ID, raises an exception."""
+
+        raise NotImplementedError
+
+    def remove_audio_features(self, song_id: str) -> bool:
+        raise NotImplementedError
+
+    def to_dataframe(self) -> pd.DataFrame:
+        raise NotImplementedError
