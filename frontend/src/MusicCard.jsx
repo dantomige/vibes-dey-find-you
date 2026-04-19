4d8bf6a7a0b467d2892bcbb913ec483c551f6894
Daniel Ige <dantomige@gmail.com>
2026-04-18 21:00:08 -0700

    feat: simple music card component

--- frontend/src/MusicCard.jsx
+++ frontend/src/MusicCard.jsx
@@ -1,9 +1,17 @@
-
 function MusicCard({ song }) {
-    // TODO
-    // each song should have an id
+  return (
+    <div className="card">
+      <h3 className="title">{song.title}</h3>
+
+      <p className="artist">
+        {song.artists?.join(", ")}
+      </p>
+
+      <p className="score">
+        Score: {song.score.toFixed(3)}
+      </p>
+    </div>
+  );
 }
-
-
 
 export default MusicCard;
