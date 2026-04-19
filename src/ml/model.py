c30fa36a48f47da337a27b58a9c8c5c28e52af45
Daniel Ige <dantomige@gmail.com>
2026-04-18 20:44:18 -0700

    feat: model architecture

--- src/ml/model.py
+++ src/ml/model.py
@@ -10,3 +10,51 @@ Allows for more complex interaction.
 * Make sure to use the same encoder when it comes to vectorizing readable text (queries, titles, genres)
 
 """
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class QueryTowerModel(nn.Module):
+    def __init__(self, input_dim, embedding_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, embedding_dim),
+        )
+
+    def forward(self, x):
+        return F.normalize(self.net(x), dim=-1)
+
+
+class SongTowerModel(nn.Module):
+    def __init__(self, input_dim, embedding_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, embedding_dim),
+        )
+
+    def forward(self, x):
+        return F.normalize(self.net(x), dim=-1)
+
+
+class TwoTowerModel(nn.Module):
+    def __init__(self, query_input_dim, song_input_dim, embedding_dim):
+        super().__init__()
+        self.query_tower = QueryTowerModel(
+            input_dim=query_input_dim, embedding_dim=embedding_dim
+        )
+        self.song_tower = SongTowerModel(
+            input_dim=song_input_dim, embedding_dim=embedding_dim
+        )
+
+    def forward(self, query_x: torch.Tensor, songs_x: torch.Tensor):
+        q = self.query_tower(query_x)
+        s = self.song_tower(songs_x)
+
+        scores = torch.matmul(s, q.T)
+        return scores
